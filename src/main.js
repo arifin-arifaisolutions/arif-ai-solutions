afcd98fb59148b4007e2f5f7552db1d6d82b2374
arifin-arifaisolutions <arifin@arifaisolutions.com>
2026-03-19 12:46:18 +0800

    feat: add constellation canvas background to hero section

--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@ import './styles/tokens.css'
 import './styles/base.css'
 import './styles/navbar.css'
 import './styles/hero.css'
+import './styles/who-we-help.css'
 import './styles/services.css'
 import './styles/marquee.css'
 import './styles/how-we-work.css'
 import './styles/about.css'
 import './styles/why-choose-us.css'
 import './styles/track-record.css'
+import './styles/faq.css'
 import './styles/contact.css'
 import './styles/footer.css'
 
@@ -18,6 +20,9 @@ import { initCursor } from './modules/cursor.js'
 import { initContactForm } from './modules/contact-form.js'
 import { initTilt } from './modules/tilt.js'
 import { initMagnetic } from './modules/magnetic.js'
+import { initConstellation } from './modules/hero-constellation.js'
+import { initServicesExpand } from './modules/services-expand.js'
+import { initScrollAnimations } from './modules/scroll-animations.js'
 import './modules/i18n.js'
 
 // Init
@@ -26,3 +31,6 @@ initCursor()
 initContactForm()
 initTilt()
 initMagnetic()
+initConstellation()
+initServicesExpand()
+initScrollAnimations()
